457ed75ae41c51ec7d35238383cfa7077480ec0d
Kasia Zadurska <kzadurska@gmail.com>
2022-08-22 10:02:32 +0200

    Setup repo

---
+++ README.md
@@ -0,0 +1,28 @@
+# line-chart-d3
+
+## Installation
+
+```bash
+git clone git@github.com:kzadurska/line-chart-d3.git
+cd line-chart-d3
+npm ci
+```
+
+## Development
+
+`npm start`
+
+The app will be available at http://0.0.0.0:8080/
+
+For production build type `npm run build`, the app will be built into the `/dist` directory.
+
+## Contributing
+
+* To lint files run `npm run lint`
+* To format code run `npm run prettier`
+
+Both accept `:fix` parameter that'll try to automatically fix whatever is possible.
+
+## Deployment
+
+TBD
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { GlobalStyles } from 'styles';
+import Button from 'components/Button';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+
+      <main css="display: flex; flex-direction: column; align-items: center; flex-wrap: wrap; position: relative;">
+        <header>
+          <h1>No elo</h1>
+        </header>
+
+        <div css="display: flex; flex-direction: row; align-items: stretch; justify-content: space-between; flex-wrap: wrap;">
+          <Button onClick={() => {}}>button</Button>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App
